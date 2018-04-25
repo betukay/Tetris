007c3b9d121db9d0765d9b2ab51f734a8f5508d9
Betu <bkay210@gmail.com>
2018-04-24 23:29:25 -0400

    add logic and style

--- tetris.js
+++ tetris.js
@@ -3,15 +3,26 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0],
-];
+function arenaSweep(){
+  let rowCount = 1;
+  outer: for(let y = arena.length - 1; y > 0; --y){
+    for(let x = 0; x < arena[y].length; ++x){
+      if (arena[y][x] === 0){
+        continue outer;
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    ++y;
+
+    player.score += rowCount * 10;
+    rowCount *= 2;
+  }
+}
 
 function collide(arena, palyer){
   const [m, o] = [player.matrix, player.pos];
-  for(let y = 0; y < matrix.length; ++y){
+  for(let y = 0; y < m.length; ++y){
     for(let x =0; x < m[y].length; ++x){
       if(m[y][x] !== 0 &&
         (arena[y + o.y] &&
@@ -31,6 +42,52 @@ function createMatrix(w, h){
   return matrix;
 }
 
+function createPiece(type){
+  if(type === 'T'){
+    return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ];
+  } else if (type === 'O'){
+    return [
+      [2, 2],
+      [2, 2],
+    ];
+  } else if (type === 'L'){
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3],
+    ];
+  } else if (type === 'J'){
+    return [
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0],
+    ];
+  } else if (type === 'I'){
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+    ];
+  } else if (type === 'S'){
+    return [
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0],
+    ];
+  } else if (type === 'Z'){
+    return [
+      [7, 7, 0],
+      [0, 7, 7],
+      [0, 0, 0],
+    ];
+  }
+}
+
 function draw(){
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -43,7 +100,7 @@ function drawMatrix(matrix, offset){
   matrix.forEach((row, y)=> {
     row.forEach((value, x)=> {
       if(value !== 0) {
-        context.fillStyle = 'red';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x,
                     y + offset.y,
                     1, 1);
@@ -67,7 +124,9 @@ function playerDrop(){
   if(collide(arena, player)){
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
+    arenaSweep();
+    updateScore();
   }
   dropCounter = 0;
 };
@@ -76,6 +135,19 @@ function playerMove(dir){
   player.pos.x += dir;
   if(collide(arena, player)){
     player.pos.x -= dir;
+  }
+}
+
+function playerReset(){
+  const pieces = 'ILJOTSZ';
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  player.pos.y = 0;
+  player.pos.x = (arena[0].length / 2 | 0) -
+                 (player.matrix[0].length / 2 | 0);
+  if (collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+    player.score = 0;
+    updateScore();
   }
 }
 
@@ -131,11 +203,27 @@ function update(time = 0){
   requestAnimationFrame(update);
 }
 
+function updateScore (){
+  document.getElementById('score').innerText = player.score;
+}
+
+const colors = [
+  null,
+  '#FF0D72',
+  '#0DC2FF',
+  '#0DFF72',
+  '#F538FF',
+  '#FF8E0D',
+  '#FFE138',
+  '#3877FF',
+]
+
 const arena = createMatrix(12, 20);
 
 const player = {
-  pos: {x:5, y:5},
-  matrix: matrix,
+  pos: {x:0, y:0},
+  matrix: null,
+  score: 0,
 }
 
 document.addEventListener('keydown', event =>{
@@ -152,4 +240,6 @@ document.addEventListener('keydown', event =>{
   }
 });
 
+playerReset();
+updateScore();
 update();
